ae102a82085a4d146fbb5d68366996277aec0b7a
Rebecca Way <rebeccaway2016@u.northwestern.edu>
2016-07-05 14:58:16 -0500

    Updated ellipse test; removed failed code
    Found a solution for rotated ellipse; deleted old code and added new

--- ellipsetest.m
+++ ellipsetest.m
@@ -3,50 +3,21 @@
 %It is meant for envisioning what an image or path will look like
 %%%%%%%%%%%%%%%%%%%%%%%%%
 
-%copied the math from https://www.mathworks.com/matlabcentral/answers/86615-how-to-plot-an-ellipse?requestedDomain=www.mathworks.com
+%Math from http://stackoverflow.com/questions/29367548/how-to-apply-rotation-to-an-ellipse-defined-by-center-and-axis-lengths
 
-x1 = -50;
-y1 = 0;
-x2 = 50;
-y2 = 0;
-e = .9;
+majorAxis = 2;
+minorAxis = 1;
+centerX = 10;
+centerY = 15;
+orientation = -120;
 
-%center
-cx = 0;
-cy = 0;
-%axis lengths
-al = 25;
-bl = 10;
+theta = 0 : 0.05 : 2*pi;
+orientation=orientation*pi/180;
+xx = (majorAxis/2) * sin(theta) + centerX;
+yy = (minorAxis/2) * cos(theta) + centerY;
 
-a = 1/2*sqrt((x2-x1)^2+(y2-y1)^2);
-b = a*sqrt(1-e^2);
-t = linspace(0,2*pi);
-X = a*cos(t);
-Y = b*sin(t);
-w = atan2(y2-y1,x2-x1);
-x = (x1+x2)/2 + X*cos(w) - Y*sin(w);
-y = (y1+y2)/2 + X*sin(w) + Y*cos(w);
-
-%rotated equations
-rx = cx + (al*cos(t));
-ry = cy + (bl*cos(t));
-
-new_x = x;
-new_y = y;
-
-
-%rotate (not transform-dependent)
-for m = 1:numel(x)
-%         if any(m == Breaks)
-%             f = randi(360);
-%         end 
-    f = 25;
-    new_x(m) = x(m)*cos(f) - x(m)*sin(f);
-    new_y(m) = y(m)*cos(f) + y(m)*sin(f);
-end
-
-
-
-%plot(x,y,new_x,new_y, '--')
-plot(x,y,rx,ry,'--')
+xx2 = (xx-centerX)*cos(orientation) - (yy-centerY)*sin(orientation) + centerX;
+yy2 = (xx-centerX)*sin(orientation) + (yy-centerY)*cos(orientation) + centerY;
+plot(xx2,yy2)
 axis equal
+grid
