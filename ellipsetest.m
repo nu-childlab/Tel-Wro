79de2846379734df1d3b7e4df52b8551229118c1
Rebecca Way <rebeccaway2016@u.northwestern.edu>
2016-07-05 17:03:32 -0500

    Updated ellipse test; points calculation done
    Ellipse test will now successfully plot any number of ellipses of the
    same size equally arranged around the origin. (Basically polar loops,
    but with ellipses that are the same size regardless of the number of
    loops.)

--- ellipsetest.m
+++ ellipsetest.m
@@ -1,53 +1,106 @@
-%%%%%%FUNCTION DESCRIPTION
-%This file is designed to test plotting ellipses
-%It is meant for envisioning what an image or path will look like
-%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%FUNCTION DESCRIPTION
+% %This file is designed to test plotting ellipses
+% %It is meant for envisioning what an image or path will look like
+% %%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% %Math from http://stackoverflow.com/questions/29367548/how-to-apply-rotation-to-an-ellipse-defined-by-center-and-axis-lengths
+% 
+% %parameters
+% majorAxis = 2;
+% minorAxis = 1;
+% centerX = 0;
+% centerY = 0;
+% circleAxis = .1;
+% 
+% orientation = -120;
+% 
+% %setup
+% theta = linspace(0,2*pi,1000);
+% orientation=orientation*pi/180;
+% 
+% %Plot a central circle)
+% circlex = (circleAxis/2) * sin(theta) + centerX;
+% circley = (circleAxis/2) * cos(theta) + centerY;
+% 
+% %Plot an ellipse
+% x = (majorAxis/2) * sin(theta) + centerX;
+% y = (minorAxis/2) * cos(theta) + centerY;
+% 
+% x2 = (x-centerX)*cos(orientation) - (y-centerY)*sin(orientation) + centerX;
+% y2 = (x-centerX)*sin(orientation) + (y-centerY)*cos(orientation) + centerY;
+% 
+% %Push out a bit (the edge should hit the origin; you'll see what I mean)
+% for m = 1:numel(x2)
+%     xx2(m) = x2(m) + (x2(round(numel(x2)*.75)) *1);
+%     yy2(m) = y2(m) + (y2(round(numel(y2)*.75)) *1);
+% end
+% 
+% %Second ellipse
+% orientation3 = 15;
+% orientation3=orientation3*pi/180;
+% 
+% x3 = (x-centerX)*cos(orientation3) - (y-centerY)*sin(orientation3) + centerX;
+% y3 = (x-centerX)*sin(orientation3) + (y-centerY)*cos(orientation3) + centerY;
+% 
+% %Push out a bit (the edge should hit the origin; you'll see what I mean)
+% for m = 1:numel(x3)
+%     xx3(m) = x3(m) + (x3(round(numel(x3)*.75)) *1);
+%     yy3(m) = y3(m) + (y3(round(numel(y3)*.75)) *1);
+% end
+% 
+% % For plotting basic ellipses
+% % plot(x2,y2,circlex,circley,'--', x3,y3,'g', xx3,yy3,'m', xx2,yy2,'c')
+% % axis equal
+% % grid
 
-%Math from http://stackoverflow.com/questions/29367548/how-to-apply-rotation-to-an-ellipse-defined-by-center-and-axis-lengths
 
-%parameters
+%%%%%%%NEXT
+%I'm gonna work on the for-loop point creation
+
+numberOfLoops = 5;
+pointsx = [];
+pointsy = [];
 majorAxis = 2;
 minorAxis = 1;
 centerX = 0;
 centerY = 0;
-circleAxis = .1;
+numberOfFrames = 100;
+theta = linspace(0,2*pi,numberOfFrames);
+orientation = linspace(0,360-round(360/numberOfLoops),numberOfLoops);
+%Note: 0 to 360-(360/numberOfLoops)?
 
-orientation = -120;
 
-%setup
-theta = linspace(0,2*pi,1000);
-orientation=orientation*pi/180;
+for i = 1:numberOfLoops
+    %orientation calculated from above
+    loopOri=orientation(i)*pi/180;
+    
+    %Start with the basic, unrotated ellipse
+    initx = (majorAxis/2) * sin(theta) + centerX;
+    inity = (minorAxis/2) * cos(theta) + centerY;
+    
+    %Then rotate it
+    x = (initx-centerX)*cos(loopOri) - (inity-centerY)*sin(loopOri) + centerX;
+    y = (initx-centerX)*sin(loopOri) + (inity-centerY)*cos(loopOri) + centerY;
+    
+    %then push it out based on the rotation
+    for m = 1:numel(x)
+        x2(m) = x(m) + (x(round(numel(x)*.75)) *1);
+        y2(m) = y(m) + (y(round(numel(y)*.75)) *1);
+    end
+    
+    %It doesn't start from the right part of the ellipse, so I'm gonna
+    %shuffle it around so it does. (this is important I promise)    
+    start = round(numberOfFrames/4);
+    x3 = [x2(start:numberOfFrames) x2(1:start-1)];
+    y3 = [y2(start:numberOfFrames) y2(1:start-1)];
 
-%Plot a central circle)
-circlex = (circleAxis/2) * sin(theta) + centerX;
-circley = (circleAxis/2) * cos(theta) + centerY;
-
-%Plot an ellipse
-x = (majorAxis/2) * sin(theta) + centerX;
-y = (minorAxis/2) * cos(theta) + centerY;
-
-x2 = (x-centerX)*cos(orientation) - (y-centerY)*sin(orientation) + centerX;
-y2 = (x-centerX)*sin(orientation) + (y-centerY)*cos(orientation) + centerY;
-
-%Push out a bit (the edge should hit the origin; you'll see what I mean)
-for m = 1:numel(x2)
-    xx2(m) = x2(m) + (x2(round(numel(x2)*.75)) *1);
-    yy2(m) = y2(m) + (y2(round(numel(y2)*.75)) *1);
+    pointsx = [pointsx x3];
+    pointsy = [pointsy y3];
+    disp(numel(pointsx))
 end
 
-%Second ellipse
-orientation3 = 15;
-orientation3=orientation3*pi/180;
+n = numel(pointsx);
 
-x3 = (x-centerX)*cos(orientation3) - (y-centerY)*sin(orientation3) + centerX;
-y3 = (x-centerX)*sin(orientation3) + (y-centerY)*cos(orientation3) + centerY;
-
-%Push out a bit (the edge should hit the origin; you'll see what I mean)
-for m = 1:numel(x3)
-    xx3(m) = x3(m) + (x3(round(numel(x3)*.75)) *1);
-    yy3(m) = y3(m) + (y3(round(numel(y3)*.75)) *1);
-end
-
-plot(x2,y2,circlex,circley,'--', x3,y3,'g', xx3,yy3,'m', xx2,yy2,'c')
+plot(pointsx, pointsy)
 axis equal
 grid
